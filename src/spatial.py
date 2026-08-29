81ec1acdf65c42459469dfb326276df20f88421a
Allanez <anamistoso@up.edu.ph>
2026-08-29 20:53:13 +0800

    Implement check spatial validity and optional meaning(name, tag) to Point

--- src/spatial.py
+++ src/spatial.py
@@ -1,6 +1,14 @@
 class Point:
-    def __init__(self, id, lon, lat):
+    def __init__(self, id, lon, lat, name=None, tag=None):
+        if not (-180 <= lon <= 180):
+            raise ValueError("Longitude must be between -180 and 180 degrees.")
+
+        if not (-90 <= lat <= 90):
+            raise ValueError("Latitude must be between -90 and 90 degrees.")
+        
         self.id = id
         self.lon = lon
         self.lat = lat
+        self.name = name
+        self.tag = tag
 
