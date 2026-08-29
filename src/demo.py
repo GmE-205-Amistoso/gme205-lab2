4988f2ff24b354e9c63969c812c8db34deac79c4
Allanez <anamistoso@up.edu.ph>
2026-08-29 21:27:40 +0800

    Implemented instance, static, and class methods to Point

--- src/demo.py
+++ src/demo.py
@@ -2,6 +2,9 @@ from spatial import Point
 
 p = Point("A", 121.0, 14.6)
 print(p.id, p.lon, p.lat)
+print(p.to_tuple())
 
-q = Point("X", 999, 14)
-print(p.id, p.lon, p.lat)
+print(p.distance_to(Point("B", 123.1, 15.7)))
+
+# q = Point("X", 999, 14)
+# print(p.id, p.lon, p.lat)
--- src/spatial.py
+++ src/spatial.py
@@ -1,3 +1,5 @@
+import math as Math
+
 class Point:
     def __init__(self, id, lon, lat, name=None, tag=None):
         if not (-180 <= lon <= 180):
@@ -12,3 +14,57 @@ class Point:
         self.name = name
         self.tag = tag
 
+    # ---------------------------------------------------------------------------
+    # Instance methods
+    # ---------------------------------------------------------------------------
+    def to_tuple(self) -> tuple[float, float]:
+        """
+        Return the coordinate as a (lon, lat) tuple.
+        """
+        return (self.lon, self.lat)
+
+    def distance_to(self, other):
+        return Point.haversine_m(self.lon, self.lat, other.lon, other.lat)
+
+    def is_poi(self):
+            return (self.tag or "").lower() == "poi"
+
+    # ---------------------------------------------------------------------------
+    # Static methods
+    # ---------------------------------------------------------------------------
+
+    @staticmethod
+    def haversine_m(lon1:float, lat1:float, lon2:float, lat2:float) -> float:
+        """
+        Calculate the Haversine distance between two lat/lon points in meters.
+        """
+        R = 6_371_000.0     #Earth radius in meters
+
+        phi1 = Math.radians(lat1)
+        phi2 = Math.radians(lat2)
+        dphi = Math.radians(lat2 - lat1)
+        dlambda = Math.radians(lon2 - lon1)
+
+        a = (
+            Math.sin(dphi / 2.0) ** 2
+            + Math.cos(phi1)
+            * Math.cos(phi2)
+            * Math.sin(dlambda / 2.0) ** 2
+        )
+        c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+        return R * c
+
+    # ---------------------------------------------------------------------------
+    # Class methods
+    # ---------------------------------------------------------------------------
+
+    @classmethod
+    def from_row(cls, row):
+        return cls(
+            id = row["id"], 
+            lon = row["lon"], 
+            lat = row["lat"], 
+            name = row.get("name"), 
+            tag = row.get("tag")
+        )
